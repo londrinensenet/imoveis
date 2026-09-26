c1421f2d5be856ca9ff960d99c05b50529a9da9b
londrinensenet <londrinense.net@gmail.com>
2026-09-26 07:00:33 -0300

    Aprimora paridade visual do portal público

--- public/assets/js/detalhe.js
+++ public/assets/js/detalhe.js
@@ -6,8 +6,8 @@ import {
   money,
   precoDo,
   urlPublica,
-} from "./common.js?v=20260926-3";
-import { criarCard } from "./modules/listagem.js?v=20260926-3";
+} from "./common.js?v=20260926-4";
+import { criarCard } from "./modules/listagem.js?v=20260926-4";
 // Dados descritivos são inseridos por textContent; substitui o antigo escape(x.descricao).
 const root = document.querySelector("#detalhe"),
   params = new URLSearchParams(location.search),
